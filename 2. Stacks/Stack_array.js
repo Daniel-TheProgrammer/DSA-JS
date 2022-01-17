7c338811dee98787564a50ff87c775109db176bd
xaster-Kies <samueledward40@gmail.com>
2022-01-17 17:37:58 +0100

    popping elements

--- 2. Stacks/Stack_array.js
+++ 2. Stacks/Stack_array.js
@@ -6,14 +6,23 @@
 class Stack {
     constructor() {
         this.items = []
+        this.count = 0;
     }
 
     push(element) {
-        this.items.push(element);
+        this.items[this.count] = element
+        this.count++;
     }
 
     pop() {
-        return this.items.pop();
+        if(this.isEmpty()) {
+            return undefined
+        }
+        this.count--;
+        const result = this.items[this.count]
+        delete this.items[this.count]
+        return result;
+
     }
 
     peek() {
@@ -21,15 +30,29 @@ class Stack {
     }
 
     isEmpty() {
-        return this.items.length === 0;
+        return this.count === 0;
     }
 
     size() {
-        return this.items.length
+        return this.count
     }
 
     clear() {
         this.items = [];
     }
 }
+
+const stack = new Stack();
+console.log(stack.isEmpty())
+
+// Calling the peek
+console.log(stack.peek())
+
+
+// Adding another element
+stack.push(11);
+console.log(stack.size())
+console.log(stack.isEmpty());
+
+
 
