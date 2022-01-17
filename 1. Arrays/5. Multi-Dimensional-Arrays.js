f68bd579ffa658dedc3b21a3ebe9a2caf9ae37ca
xaster-Kies <samueledward40@gmail.com>
2022-01-17 12:38:37 +0100

    initializing average temp

---
+++ 1. Arrays/5. Multi-Dimensional-Arrays.js
@@ -0,0 +1,25 @@
+/* Author: Samuel Tiokeng
+*  Description: Multi-Dimensional Arrays  
+*  Date: 17th January 2022
+*/
+
+let averageTempDay1 = [72, 75, 79, 81, 81]
+let averageTempDay2 = [81, 79, 75, 73, 72]
+
+// Day 1
+averageTemp[0] = [];
+averageTemp[0][0] = 72;
+averageTemp[0][1] = 75;
+averageTemp[0][2] = 79;
+averageTemp[0][3] = 79;
+averageTemp[0][4] = 81;
+averageTemp[0][5] = 81;
+
+//Day 2
+averageTemp[0] = [];
+averageTemp[1][0] = 81;
+averageTemp[1][1] = 79;
+averageTemp[1][2] = 75;
+averageTemp[1][3] = 75;
+averageTemp[1][4] = 73;
+averageTemp[1][5] = 73;
