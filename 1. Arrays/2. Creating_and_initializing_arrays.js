ced0d4fa71cd9286a4714d38346c259c52e63700
xaster-Kies <samueledward40@gmail.com>
2022-01-17 01:26:42 +0100

    Creating and initializing array

---
+++ 1. Arrays/2. Creating_and_initializing_arrays.js
@@ -0,0 +1,17 @@
+/* Author: Samuel Tiokeng
+*  Description: Creating and Initializing arrays
+*  Date: 17th January 2022
+*/
+
+let daysOfWeek = new Array();
+dayOfWeek = new Array(7);
+daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+console.log(daysOfWeek);
+
+
+// Accessing elements and iterating an array
+
+for(let i = 0; i < daysOfWeek.length; i++) {
+    console.log(daysOfWeek[i])
+}
