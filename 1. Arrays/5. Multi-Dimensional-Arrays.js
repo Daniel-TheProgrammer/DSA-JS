6a2e8fbcfc112f4f673c2fb2ac2d8f7cb68bc7a3
xaster-Kies <samueledward40@gmail.com>
2022-01-17 12:53:23 +0100

    print matrix

--- 1. Arrays/5. Multi-Dimensional-Arrays.js
+++ 1. Arrays/5. Multi-Dimensional-Arrays.js
@@ -23,3 +23,11 @@ averageTemp[1][2] = 75;
 averageTemp[1][3] = 75;
 averageTemp[1][4] = 73;
 averageTemp[1][5] = 73;
+
+function printMatrix(myMatrix) {
+    for(let i = 0; i < myMatrix.length; i++) {
+        for(let j = 0; j < myMatrix[i].length; j++) {
+            console.log(myMatrix[i][j])
+        }
+    }
+}
